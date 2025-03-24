7d8c9a9d30b1a573aebc7ad2149965fc2303a3dd
JCampo1502 <javierdavidcamposuarez15@gmail.com>
2025-03-24 13:08:20 -0500

    feat: :sparkles: Estudio de caso 02

--- estudioDeCaso.cpp
+++ estudioDeCaso.cpp
@@ -14,8 +14,10 @@ void clearConsole();
 int main()
 {
     string referencia, descripcion;
-    int talla, costo, precioVenta;
+    int talla, cantidadDeZapatos;    
     char disponibie;
+    double costoUnitario,precioUnitario;
+
     cout << "ADMINISTRAR VENTA DE ZAPATOS" << endl;
     cout << "Digite la referencia del zapato... "<<endl;
     cin >> referencia;
@@ -25,18 +27,29 @@ int main()
     cin >> talla;
     cout << "Digite la letra si esta disponible o no para la venta S/N... "<<endl;
     cin >> disponibie;
-    cout << "Digite el costo del zapato... "<<endl;
-    cin >> costo;
-    cout << "Digite el precio de venta del zapato... "<<endl;
-    cin >> precioVenta;
+    cout << "Digite la cantidad de zapatos... "<<endl;
+    cin >> cantidadDeZapatos;
+    cout << "Digite el costo unitario del zapato... "<<endl;
+    cin >> costoUnitario;
+    cout << "Digite el precio unitario de venta del zapato... "<<endl;
+    cin >> precioUnitario;
+    
+
     clearConsole();
     cout << "LOS DATOS REGISTRADOS SON LOS SIGUIENTES: " << endl;
     cout << "REFERENCIA: " << referencia << endl;
     cout << "DESCRIPCION: " << descripcion << endl;
     cout << "TALLA: " << talla << endl;
     cout << "DISPONIBILIDAD: " << disponibie << endl;
-    cout << "COSTO: " << costo << endl;
-    cout << "PRECIO: " << precioVenta << endl << endl;
+    cout << "CANTIDAD DE ZAPATOS: " << cantidadDeZapatos << endl;
+    cout << "COSTO UNIDAD: " << costoUnitario << endl;
+    cout << "COSTO TOTAL: " << (costoUnitario * cantidadDeZapatos) << endl;
+    cout << "PRECIO UNIDAD: " << precioUnitario << endl;
+    cout << "PRECIO TOTAL DE "<< cantidadDeZapatos << " UNIDADES: " << (precioUnitario * cantidadDeZapatos) << endl;
+    cout << "UTILIDAD POR UNIDAD: " << (precioUnitario - costoUnitario) << endl;
+    cout << "UTILIDAD TOTAL: " << ((precioUnitario - costoUnitario) * cantidadDeZapatos) << endl;
+    cout << "UTILIDAD PORCENTUAL: " << (((precioUnitario - costoUnitario) / costoUnitario) * 100) << "%" << endl<< endl;
+    
     cout << "Gracias por digitar la informacion" << endl;
     cout << "By Javier David Campo Suarez" << endl << endl << endl;
 
